1f747d980816333a703d35d5bc5834d59055176b
ejgavin <ejgavin11@gmail.com>
2025-05-04 11:31:33 -0400

    Initial commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', async (req, res) => {
     const headers = {
       'Referer': origin,
       'Origin': origin,
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36', // Updated User-Agent
     };
 
     const response = await axios.get(targetUrl, { headers });
